4120500de78b451527381af3ad3387ac93f02ca2
Shu Xenos <alex.halbot2@gmail.com>
2026-05-28 16:15:25 +0200

    Dynamic players system

--- src/supabase.js
+++ src/supabase.js
@@ -1,13 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-console.log(import.meta.env.VITE_SUPABASE_URL);
-console.log(import.meta.env.VITE_SUPABASE_KEY);
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(
   supabaseUrl,
   supabaseKey
-)
+);
